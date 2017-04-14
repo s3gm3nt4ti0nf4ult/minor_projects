4d63ca744216800ba0860ab35c4f44dc242373f6
Root <programista15k@tutanota.com>
2017-04-14 18:07:26 -0400

    cleanup and basic PF flag usage

--- puthchar.asm
+++
@@ -1,9 +0,0 @@
-[bits 32]
-; cdcel convention
-
-push 'H'
-call [ebx+4]  ;putchar
-add esp, 4      ;adding to stack = removing from stack 'H' that was pushed before
-
-push 0
-call [ebx]  ;calling exit 
